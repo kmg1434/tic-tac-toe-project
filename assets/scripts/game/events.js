c8a04304846d4c5022547820da0460cfc8bc2ae4
kmg1434 <kmg1434@gmail.com>
2016-11-02 14:13:38 -0400

    Finished winCheck and tie game check
    tie game prints to console successfully
    next: visual win/tie response

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -13,16 +13,42 @@ const glob = require('./global.js');
 const winCheck = function () {
 
   let b = glob.vars.board;
+  let gameOver = false;
 
   // HORIZONTAL CHECKS
   if (b[0] && (b[0] === b[1]) && (b[1] === b[2])) {
     console.log(b[0] + ' wins!');
+    gameOver = true;
   } else if (b[3] && (b[3] === b[4]) && (b[4] === b[5])) {
     console.log(b[3] + ' wins!');
+    gameOver = true;
   } else if (b[6] && (b[6] === b[7]) && (b[7] === b[8])) {
     console.log(b[6] + ' wins!');
+    gameOver = true;
   }
 
+  // VERTICAL CHECKS
+  if (b[0] && (b[0] === b[3]) && (b[3] === b[6])) {
+    console.log(b[0] + ' wins!');
+    gameOver = true;
+  } else if (b[1] && (b[1] === b[4]) && (b[4] === b[7])) {
+    console.log(b[1] + ' wins!');
+    gameOver = true;
+  } else if (b[2] && (b[2] === b[5]) && (b[5] === b[8])) {
+    console.log(b[2] + ' wins!');
+    gameOver = true;
+  }
+
+  // DIAGONAL CHECKS
+  if (b[0] && (b[0] === b[4]) && (b[4] === b[8])) {
+    console.log(b[0] + ' wins!');
+    gameOver = true;
+  } else if (b[2] && (b[2] === b[4]) && (b[4] === b[6])) {
+    console.log(b[2] + ' wins!');
+    gameOver = true;
+  }
+
+  return gameOver;
 };
 
 const onClick = function (event) {
@@ -32,28 +58,31 @@ const onClick = function (event) {
   let tile = $(this).attr('id');
   let tileId = '#' + tile;
   let i = +(tile.replace(/\D/g, '')); // tile number
-  console.log('Clicked tile ' + i);
 
   if (!glob.vars.board[i]) { // if not yet clicked
-
     if (glob.vars.xTurn) {
-
       $(tileId).html('X');
       glob.vars.board[i] = 'x';
-
     } else {
       $(tileId).html('O');
       glob.vars.board[i] = 'o';
-
     }
-
-    winCheck();
-
-    glob.vars.xTurn = !glob.vars.xTurn; // change teams
 
   }
 
-  console.log('X turn? ' + glob.vars.xTurn);
+  if (!winCheck()) {
+    glob.vars.xTurn = !glob.vars.xTurn; // change teams
+    glob.vars.turnCount++;
+    if (glob.vars.turnCount === 9) {
+      console.log('TIE GAME');
+    }
+
+  }
+
+  // if ((glob.vars.turnCount === 9)) {
+  //   console.log("TIE GAME!!!!!!!!");
+  // }
+
   console.table(glob.vars.board);
 
 };
